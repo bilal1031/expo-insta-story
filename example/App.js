9cdcb14a714133e108deced88069101f456cedca
Muhammad Bilal <56048741+bilal1031@users.noreply.github.com>
2025-10-29 23:30:34 +0500

    Updated dependencies and peer dependencies in package.json, modified app.json for bundle identifier and new architecture support, adjusted App.js layout, and improved package.json scripts for Expo commands. Removed outdated yarn.lock file.

--- example/App.js
+++ example/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import ExpoInstaStory from 'expo-insta-story';
 
 export default function App() {
@@ -41,7 +41,7 @@ export default function App() {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <StatusBar style="auto" />
       <ExpoInstaStory
         data={createData()}
@@ -61,13 +61,14 @@ export default function App() {
         }}
         style={{ marginTop: 30 }}
       />
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
+    paddingTop: 30,
   },
 });
